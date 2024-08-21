b8c2cb303047a9a1bf6ab1f8eee86e3ae3bb4640
ael-mank <ael-mank@student.42.fr>
2024-08-21 14:32:08 +0200

    norm bvh, need to make the main function shorter or divide it

--- src/aabbbvh/bvh.c
+++ src/aabbbvh/bvh.c
@@ -6,7 +6,7 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 11:28:31 by ael-mank          #+#    #+#             */
-/*   Updated: 2024/08/20 22:36:38 by ael-mank         ###   ########.fr       */
+/*   Updated: 2024/08/21 14:31:39 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 //     if (node == NULL) {
 //         print_indent(level);
 //         printf("NULL\n");
-//         return;
+//         return ;
 //     }
 
 //     print_indent(level);
@@ -54,72 +54,47 @@
 int	bvh_hit_check_box(t_bvh *node, t_ray r, t_interval ray_t)
 {
 	if (!node->box.hit(r, node->box, ray_t))
-	{
 		return (0);
-	}
 	return (1);
 }
 
 int	bvh_hit_node(t_bvh *node, t_ray r, t_interval ray_t, t_hitrecord *rec)
 {
 	t_hitrecord	left_rec;
-	int			hit_left;
-	t_interval	right_t;
 	t_hitrecord	right_rec;
-	int			hit_right;
+	t_interval	right_t;
 
+	int (hit_left) = 0;
+	int (hit_right) = 0;
+	right_t = ray_t;
 	if (node->object && !node->left && !node->right)
-	{
 		return (node->object->hit(r, node->object->object, ray_t, rec));
-	}
-	hit_left = 0;
 	if (node->left)
-	{
 		hit_left = bvh_hit(node->left, r, ray_t, &left_rec);
-	}
 	if (hit_left)
-	{
 		right_t = (t_interval){ray_t.min, left_rec.t};
-	}
-	else
-	{
-		right_t = ray_t;
-	}
-	hit_right = 0;
 	if (node->right)
-	{
 		hit_right = bvh_hit(node->right, r, right_t, &right_rec);
-	}
 	if (hit_left && hit_right)
 	{
 		if (left_rec.t < right_rec.t)
-		{
 			*rec = left_rec;
-		}
 		else
-		{
 			*rec = right_rec;
-		}
-		return (1);
 	}
 	else if (hit_left)
-	{
 		*rec = left_rec;
-		return (1);
-	}
 	else if (hit_right)
-	{
 		*rec = right_rec;
-		return (1);
-	}
-	return (0);
+	else
+		return (0);
+	return (1);
 }
 
 int	bvh_hit(t_bvh *node, t_ray r, t_interval ray_t, t_hitrecord *rec)
 {
 	if (!node)
 		return (0);
-
 	if (!bvh_hit_check_box(node, r, ray_t))
 		return (0);
 	return (bvh_hit_node(node, r, ray_t, rec));

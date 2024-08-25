a46c75fc085dd5ee0d06ced6b613af12b237ce23
ael-mank <ael-mank@student.42.fr>
2024-08-25 05:16:33 +0200

    todo

--- src/init/init.c
+++ src/init/init.c
@@ -6,7 +6,7 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 21:31:07 by ael-mank          #+#    #+#             */
-/*   Updated: 2024/08/23 23:05:43 by ael-mank         ###   ########.fr       */
+/*   Updated: 2024/08/24 18:26:31 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ void	init_viewport(t_camera *camera, t_render *render)
 {
 	double	h;
 
-	camera->lookfrom = vec3(0, 3, -10);
-	camera->lookat = vec3(0, 1, 0);
+	camera->lookfrom = vec3(13, 2, 3);
+	camera->lookat = vec3(0, 0, 0);
 	camera->v_up = vec3(0, 1, 0);
 	camera->fov = 20;
 	camera->defocus_angle = 0;
@@ -46,8 +46,8 @@ void	init_viewport(t_camera *camera, t_render *render)
 			render->image_width);
 	camera->pixel_delta_v = vector_divide(camera->viewport_v,
 			render->image_height);
-	camera->samples_per_pixel = 1;
-	camera->max_depth = 3;
+	camera->samples_per_pixel = 14;
+	camera->max_depth = 55;
 }
 
 void	init_camera(t_camera *camera)
--- src/init/init_objects.c
+++ src/init/init_objects.c
@@ -6,7 +6,7 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 18:33:28 by ael-mank          #+#    #+#             */
-/*   Updated: 2024/08/24 15:27:38 by ael-mank         ###   ########.fr       */
+/*   Updated: 2024/08/24 18:26:55 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,51 +32,18 @@ t_object	*init_objects(void)
 {
 	t_object	*head;
 	t_sphere	*sphere;
-	// double		choose_mat;
-	// t_vec3		center;
-	// t_vec3		albedo;
 
 	head = NULL;
-	sphere = create_sphere(vec3(0, 1, 1000), 1000, MATTE, vec3(0, 0, 0));
+	sphere = create_sphere(vec3(0, -1000, -1), 1000, MATTE, vec3(0.8, 0.8,
+				0.0));
 	head = add_sphere(head, sphere);
-	sphere = create_sphere(vec3(0.4, 0.8, 0), 1, GLOBE, vec3(0.7, 0.6, 0.5));
+	sphere = create_sphere(vec3(0, 1, 0), 1.0, GLASS, vec3(1.0, 1.0, 1.0));
 	head = add_sphere(head, sphere);
-	// sphere = create_sphere(vec3(-1, 1.7, 0), 0.5, MOON, vec3(0.7, 0.6, 0.5));
-	// head = add_sphere(head, sphere);
-	// for (int a = -11; a < 11; a++)
-	// {
-	// 	for (int b = -11; b < 11; b++)
-	// 	{
-	// 		choose_mat = random_double();
-	// 		center = vec3(a + 0.9 * random_double(), 0.2, b + 0.9
-	// 				* random_double());
-	// 		if (sqrt(pow(center.x - 4, 2) + pow(center.y - 0.2, 2)
-	// 				+ pow(center.z, 2)) > 0.9)
-	// 		{
-	// 			if (choose_mat < 0.8)
-	// 			{
-	// 				albedo = vec3(random_double(), random_double(),
-	// 						random_double());
-	// 				albedo = vec3(albedo.x * random_double(), albedo.y
-	// 						* random_double(), albedo.z * random_double());
-	// 				sphere = create_sphere(center, 0.2, MATTE, albedo);
-	// 				head = add_sphere(head, sphere);
-	// 			}
-	// 			else if (choose_mat < 0.95)
-	// 			{
-	// 				albedo = vec3(0.5 + 0.5 * random_double(), 0.5 + 0.5
-	// 						* random_double(), 0.5 + 0.5 * random_double());
-	// 				sphere = create_sphere(center, 0.2, METAL, albedo);
-	// 				head = add_sphere(head, sphere);
-	// 			}
-	// 			else
-	// 			{
-	// 				sphere = create_sphere(center, 0.2, GLASS, vec3(1.0, 1.0,
-	// 							1.0));
-	// 				head = add_sphere(head, sphere);
-	// 			}
-	// 		}
-	// 	}
-	// }
+	sphere = create_sphere(vec3(0, 1, 0), 0.9, BUBBLE, vec3(1.0, 1.0, 1.0));
+	head = add_sphere(head, sphere);
+	sphere = create_sphere(vec3(-4, 1, 0), 1, MATTE, vec3(0.4, 0.2, 0.1));
+	head = add_sphere(head, sphere);
+	sphere = create_sphere(vec3(4, 1, 0), 1, METAL, vec3(0.8, 0.8, 0.9));
+	head = add_sphere(head, sphere);
 	return (head);
 }
